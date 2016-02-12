3e375462a5aebf4cd78e271a3d11d2b7a8ffaf89
Ivan Zahariev <famzah@famzah.net>
2016-02-12 16:33:49 +0200

    Initial commit

---
+++ AutoScreenCaptureFV/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 299);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(547, 300);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(645, 300);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            43200,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Choose";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(93, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 325);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 281);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Folder to save images:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Interval (sec):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(642, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Max image count:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Preview of the last snapshot:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(709, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "0 = disable";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 24);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(139, 19);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(645, 325);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Clean now";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 3600000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Auto screen capture FV";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 378);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Auto screen capture FV";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ AutoScreenCaptureFV/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections;
+using System.Text.RegularExpressions;
+using Microsoft.VisualBasic.FileIO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private DateTime start_timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            String saved_path = RegistryHelper.GetSetting("Settings", "RootFolder", "");
+            if (saved_path == "")
+            {
+                textBox1.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+            else
+            {
+                textBox1.Text = saved_path;
+            }
+
+            showStatus("Stopped", false);
+            numericUpDown1.Value = Convert.ToInt32(RegistryHelper.GetSetting("Settings", "Interval", "60"));
+            numericUpDown2.Value = Convert.ToInt32(RegistryHelper.GetSetting("Settings", "Cleanup", "10080"));
+        }
+
+        private Bitmap Get_screen()
+        {
+            Size s = Screen.PrimaryScreen.Bounds.Size;
+            Bitmap bt = new Bitmap(s.Width, s.Height);
+            Graphics g = Graphics.FromImage(bt);
+            g.CopyFromScreen(0, 0, 0, 0, s);
+            return bt;
+        }
+
+        private void showStatus(String message, Boolean is_error)
+        {
+            toolStripStatusLabel1.Text = message;
+            toolStripStatusLabel1.ForeColor = is_error ? Color.Red : Color.Black;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap bt;
+            bt = Get_screen();
+            pictureBox1.Image = bt;
+
+            start_timer = DateTime.Now;
+
+            String rootdir = textBox1.Text.Trim();
+            if (rootdir.Length == 0)
+            {
+                showStatus("Snapshot not saved", true);
+                return;
+            }
+
+            String timestamp = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"); // 2012-03-27-22-43-02
+            String savefile = Path.Combine(rootdir, "screenshot-" + timestamp + ".jpg");
+
+            if (File.Exists(savefile))
+            {
+                showStatus("Error, snapshot file already exists: " + savefile, true);
+                return;
+            }
+
+            try
+            {
+                bt.Save(savefile, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                showStatus("Error saving snapshot file: " + savefile, true);
+                return;
+            }
+
+            showStatus("Snapshot saved: " + savefile, false);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1_Tick(null, null);
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value)*1000;
+            start_timer = DateTime.Now;
+            toolStripProgressBar1.Maximum = timer1.Interval / 1000; // in seconds
+            toolStripProgressBar1.Minimum = 0;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = false;
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            RegistryHelper.SaveSetting("Settings", "Interval", numericUpDown1.Value.ToString());
+            RegistryHelper.SaveSetting("Settings", "Cleanup", numericUpDown2.Value.ToString());
+            RegistryHelper.SaveSetting("Settings", "RootFolder", textBox1.Text);
+            button4.Enabled = true;
+            timer3.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            timer2.Enabled = false;
+            toolStripProgressBar1.Value = 0;
+            timer1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = true;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            button4.Enabled = false;
+            showStatus("Stopped", false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBox1.Text;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                toolStripProgressBar1.Value = (DateTime.Now - start_timer).Seconds;
+            }
+            catch (System.ArgumentOutOfRangeException ex)
+            {
+                // sometimes we overflow with a bit
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+
+            string[] files = Directory.GetFiles(textBox1.Text.Trim());
+            Array.Sort(files, (a, b) => String.Compare(b, a)); // NEWEST are first
+
+            ArrayList imgfiles = new ArrayList();
+            Regex re_imgfile = new Regex("screenshot-\\d{4}-\\d{2}-\\d{2}-\\d{2}-\\d{2}-\\d{2}\\.jpg");
+            foreach (string full_name in files)
+            {
+                string short_name = Path.GetFileName(full_name);
+                if (re_imgfile.IsMatch(short_name) == false)
+                {
+                    continue;
+                }
+                imgfiles.Add(full_name);
+            }
+
+            int keep_count = Convert.ToInt32(numericUpDown2.Value);
+            if (keep_count > 0)
+            {
+                if (imgfiles.Count > keep_count)
+                {
+                    imgfiles.RemoveRange(0, keep_count);
+                    foreach (string filename in imgfiles)
+                    {
+                        //File.Delete(filename);
+                        FileSystem.DeleteFile(filename, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+                    }
+                }
+            }
+
+            button4.Enabled = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            button4_Click(null, null);
+            if (timer1.Enabled == true)
+            {
+                timer3.Enabled = true;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.BalloonTipText = "The capture is still working in background.";
+                notifyIcon1.BalloonTipTitle = this.Text;
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Info; 
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(1000);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // if we received a FormClosing event and are still minimized
+            // this means that we're probably forcibly closing the application
+            // like when the Windows account is logged off, or when Windows
+            // is being shut down
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                return; // just close the application
+            }
+
+            DialogResult res = MessageBox.Show(
+                    "You are about to end the program. Are you sure you want to continue?",
+                    "Confirm close",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button2
+            );
+            if (res == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+
+    // http://www.codeproject.com/Articles/16274/Saving-Registry-Settings
+    public class RegistryHelper
+    {
+        private static string FormRegKey(string sSect)
+        {
+            return sSect;
+        }
+        public static void SaveSetting(string Section, string Key, string Setting)
+        {
+
+            string text1 = FormRegKey(Section);
+            RegistryKey key1 =
+
+            Application.UserAppDataRegistry.CreateSubKey(text1);
+            if (key1 == null)
+            {
+                return;
+            }
+            try
+            {
+                key1.SetValue(Key, Setting);
+            }
+            catch (Exception exception1)
+            {
+                return;
+            }
+            finally
+            {
+                key1.Close();
+            }
+
+        }
+        public static string GetSetting(string Section, string Key, string Default)
+        {
+            if (Default == null)
+            {
+                Default = "";
+            }
+            string text2 = FormRegKey(Section);
+            RegistryKey key1 = Application.UserAppDataRegistry.OpenSubKey(text2);
+            if (key1 != null)
+            {
+                object obj1 = key1.GetValue(Key, Default);
+                key1.Close();
+                if (obj1 != null)
+                {
+                    if (!(obj1 is string))
+                    {
+                        return null;
+                    }
+                    return (string)obj1;
+                }
+                return null;
+            }
+            return Default;
+        }
+    }
+}
